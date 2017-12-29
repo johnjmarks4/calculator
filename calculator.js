094f4cea7652909654fca6a8b66b763ea2fa022c
John Marks <johnjmarks4@gmail.com>
2017-12-29 16:10:10 -0600

    Add operations

--- calculator.js
+++ calculator.js
@@ -1,5 +1,34 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operator(op, num1, num2) {
+  switch(op) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
+
 container = document.querySelector('#container');
 container.setAttribute("style", "width: 1060px");
 paragraph = document.createElement('p');
-paragraph.textContent = "My paragraph";
+paragraph.textContent = operator('-', 1, 1);
 container.appendChild(paragraph);
